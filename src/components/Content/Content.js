30ba1e5ff186be1047eab3c3bc8a97913a8f8eee
nhandt2101 <21020371@vnu.edu.vn>
2023-05-05 21:52:24 +0700

    lan thu n

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,52 +1,63 @@
-import axios from "axios"
-import { useState } from "react"
-import { useEffect } from "react"
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-
+import "./Content.css";
+import SearchBar from "./SearchBar.js";
+// import AddIcon from '@mui/icons-material/Add';
 export default function Content() {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
+  function logout() {
+    window.localStorage.removeItem("token");
+    navigation("/");
+  }
 
-    function logout() {
-        window.localStorage.removeItem("token");
+  function getData() {
+    console.log(`Bearer ${window.localStorage.getItem("token")}`);
+    axios
+      .get("http://localhost:8080/api/content", {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
         navigation("/");
-    }
-    
-    function getData() {
-        console.log(`Bearer ${window.localStorage.getItem("token")}`);
-        axios.get("http://localhost:8080/api/content", {
-            headers: {
-                'Authorization': `Bearer ${window.localStorage.getItem("token")}`
-            }
-        })
-        .then(res => {
-            setData(res.data);
-            console.log(res.data);
-        })
-        .catch(error => {
-            navigation("/");
-        })
-    }
+      });
+  }
 
-    useEffect(() => {
-        getData();
-      }, [])
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-        <div className="ContentWrapper">
+  return (
+    <div className="todoapp-work">
+      <div className="content-wrapper">
+        <div className="userInfo">
+            <p>Pham Ba Danh</p>
+            <button onClick={logout}>Logout</button>
+        </div>
         
-            <button onClick={logout} >Logout</button>
-            <ul>
-                {data.map(taskList => (
-                    <li key={taskList.id}>
-                        {taskList.name}
-                    </li>
-                ))}
-            </ul>
+        <div className="search-bar">
+          <SearchBar />
         </div>
 
-    )
+        <ul>
+          {data.map((taskList) => (
+            <li key={taskList.id}>{taskList.name}</li>
+          ))}
+        </ul>
+
+        <div className="add-new-task">
+          <input type="text"></input>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Content/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBar.css';
+export default function SearchBar() 
+{
+    return (
+        <input
+        type = 'text'
+        placeholder = "Search..."></input>
+    )
+}
