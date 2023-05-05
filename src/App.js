d1630783e718232be77e3f7c79d9e03b24d5b140
nhandt2101 <21020371@vnu.edu.vn>
2023-05-05 20:42:05 +0700

    new Register

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes } from 'react-router-dom';
 
 import HomePage from './components/HomePage/HomePage';
 import Content from './components/Content/Content';
+import { Register } from './components/Register/Register';
 
 function Test() {
   return (
@@ -40,6 +41,7 @@ function App() {
             <Route path='/content' element={<Content />}>
               
             </Route>
+            <Route path='/register' element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
       
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 export default function HomePage() {
-
+    const [errorLogin, setErrorLogin] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -19,18 +19,23 @@ export default function HomePage() {
         })
         .then(res => {
             navigation("/content")
+            
         })
         .catch(error => {
+            
             window.localStorage.removeItem("token");
+
         })
     }, [])
 
-    function submit() {
+    function submit(event) {
+        event.preventDefault();
         const data = {
             "username": username,
             "password": password
         }
-
+        console.log(data.username);
+        console.log(data.password);
         axios.post("http://localhost:8080/api/authenticate", data)
         .then(res => {
             // console.log(res.data.id_token)
@@ -40,6 +45,8 @@ export default function HomePage() {
         })
         .catch(err => {
             console.log("error");
+            setErrorLogin("Your username or password is inscorrect");
+            
         })
     }
 
@@ -53,13 +60,14 @@ export default function HomePage() {
 
     return (
         <div className = "formLogin">
-            <div className = "wrapper">
+            <div className = "wrapper-login">
                 <div className = "form-box-login">
                     <h2>Login</h2>
                     <form onSubmit={submit}>
                     <div className = "input-box">
                     <input type="text"
                     required
+                    value = {username}
                     onChange={handleUsername}/>
                     <label>Username</label>
                     </div>
@@ -67,10 +75,17 @@ export default function HomePage() {
                     <input 
                     type="password" 
                     required
+                    value = {password}
                     onChange={handlePassword}/>
                     <label>Password</label>
                     </div>
                     <button type = "submit" className = "btn">Login</button>
+                    <div className = "error-msg">
+                        <p>{errorLogin}</p>
+                    </div>
+                    <div class = "login-register"><p>Don't have an account? 
+                    <a href = "/register" class = "register-link">Register</a>
+                    </p></div>
                     </form>
                     </div>
             </div>
